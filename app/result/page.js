8e4e0212274c30ee74e3230ee5e83fc1c5e10c4c
gizemnkorkmaz <gizemkorkmaz@teknasyon.com>
2024-06-18 09:46:09 +0300

    refactor

--- app/result/page.js
+++ app/result/page.js
@@ -1,27 +1,25 @@
 "use client";
 
 import { useContext } from "react";
-import { useRouter } from "next/navigation";
 import { QuizContext } from "@/context/QuizContext";
 
 export default function Result() {
-  const router = useRouter();
   const { quizData } = useContext(QuizContext);
 
-  const { score, total } = quizData;
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-6xl font-bold mb-8">Quiz Results</h1>
-      <p className="text-2xl mb-4">
-        Your Score: {score} / {total}
-      </p>
-      <button
-        onClick={() => router.push("/")}
-        className="mt-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded"
-      >
-        Back to Home
-      </button>
-    </div>
+    quizData && (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+        <h1 className="text-6xl font-bold mb-8">Quiz Results</h1>
+        <p className="text-2xl mb-4">
+          Your Score: {score} / {total}
+        </p>
+        <button
+          onClick={() => router.push("/")}
+          className="mt-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded"
+        >
+          Back to Home
+        </button>
+      </div>
+    )
   );
 }
